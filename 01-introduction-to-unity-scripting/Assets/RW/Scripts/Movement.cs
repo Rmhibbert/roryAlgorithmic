094d02797f2c51cac146936cc6d30e26784572ce
Rmhibbert <127159806+Rmhibbert@users.noreply.github.com>
2024-03-12 14:18:36 +1300

    Add files via upload

---
+++ 01-introduction-to-unity-scripting/Assets/RW/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 30f;
+    public GameObject endZone;
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+
+
+    void movement()
+    {   
+       transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider endzone)
+    {
+        Destroy(gameObject);
+    }
+   
+}
---
+++ 01-introduction-to-unity-scripting/Assets/RW/Scripts/rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+
+    public float rotationSpeedY;
+    public float rotationSpeedX;
+    public float rotationSpeedZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeedX, rotationSpeedY * Time.deltaTime, rotationSpeedX);
+    }
+}
